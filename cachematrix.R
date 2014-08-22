db4649e3fac816b89bdc3b3d706577f35553b50e
hakanh <hansson392@gmail.com>
2014-08-22 19:01:06 +0200

    sista filen

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,23 @@
 ## The functions will inverse a matrix
 
 ## This function creates a special "matrix" object that can cache its inverse.
+##The input can be any matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  y<-makeVector(x)
+  
+  z<-cachemean(y)
+  
 }
 
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+##The input is the output from makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        y<-solve(x)
+ 
+  x<-solve(z)
+
 }
+
